23589a3dc419dbb502c02bcd7da662482d694d1b
HanselFaren <91465998+HanselFaren@users.noreply.github.com>
2023-03-10 19:18:00 +0700

    Add files via upload

---
+++ Homework(ToDoList)/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Intro from './components/intro';
+import data from './data.json';
+import List from './components/list';
+import Form from './components/form';
+import React, {useState} from 'react';
+
+function App() {
+    
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+  
+  return (
+    <div className="App">
+        <Intro></Intro>
+        <Form addTask={addTask}/>
+        <List toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+    </div>
+  );
+}
+
+export default App;
